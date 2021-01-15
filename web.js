1f4d93dcfc572affe4a4db5adcba9cb397181c00
marcin03 <marcinmulawa92@gmail.com>
2021-01-15 23:49:21 +0100

    third commit

--- web.js
+++ web.js
@@ -1,6 +1,17 @@
-var http = require('http');
 var PORT = process.env.PORT || 3000;
+var express = require("express");
+var app = express();
 
-http.createServer(function (req, res) {
-  res.end('Hello World');
-}).listen(PORT);
+app.get("/", function(req,res){
+	res.send("Hello world");
+});
+app.listen(PORT);
+
+
+
+
+
+
+//	res.sendFile(path.join(_dirname+'/index.html'));
+
+//var path = require('path');
